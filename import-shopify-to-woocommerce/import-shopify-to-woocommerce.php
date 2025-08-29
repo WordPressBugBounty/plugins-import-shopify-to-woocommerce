ab87dabb7dc3112f2843692c7ba4a252f926727e
Sean Murphy <sean@iamseanmurphy.com>
2025-08-29 09:18:13 +0000

    1.2.9

--- import-shopify-to-woocommerce/import-shopify-to-woocommerce.php
+++ import-shopify-to-woocommerce/import-shopify-to-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: S2W - Import Shopify to WooCommerce
  * Plugin URI: https://villatheme.com/extensions/import-shopify-to-woocommerce
  * Description: Easily migrate all Shopify products and their collections(categories) to WooCommerce after several clicks
- * Version: 1.2.8
+ * Version: 1.2.9
  * Author: VillaTheme
  * Author URI: https://villatheme.com
  * License:           GPL v2 or later
@@ -13,14 +13,14 @@
  * Copyright 2019-2025 VillaTheme.com. All rights reserved.
  * Tested up to: 6.8.2
  * WC requires at least: 7.0.0
- * WC tested up to: 10.0.4
+ * WC tested up to: 10.1.2
  * Requires PHP: 7.0
  * Requires Plugins: woocommerce
  **/
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-define( 'VI_IMPORT_SHOPIFY_TO_WOOCOMMERCE_VERSION', '1.2.8' );
+define( 'VI_IMPORT_SHOPIFY_TO_WOOCOMMERCE_VERSION', '1.2.9' );
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 add_action( 'before_woocommerce_init', function () {
 	if ( class_exists( '\Automattic\WooCommerce\Utilities\FeaturesUtil' ) ) {
--- import-shopify-to-woocommerce/readme.txt
+++ import-shopify-to-woocommerce/readme.txt
@@ -5,7 +5,7 @@ Tags: import, migration, SHOPIFY, woocommerce
 Requires at least: 5.0.0
 Tested up to: 6.8.2
 WC requires at least: 7.0
-WC tested up to: 10.0.4
+WC tested up to: 10.1.2
 Requires PHP: 7.0
 Stable tag: trunk
 License: GPLv2 or later
@@ -234,6 +234,11 @@ Preview S2W - Import Shopify to WooCommerce
 6. Webhooks
 
 == Changelog ==
+/**1.2.9 - 2025.08.29**/
+– Updated: Compatible with WC 10.1.2
+- Updated: Shopify API version 2025-07
+– Updated: Update support class
+
 /**1.2.8 - 2025.08.15**/
 – Updated: Compatible with WP 6.8.2 and WC 10.0.4
 – Updated: Update support class
